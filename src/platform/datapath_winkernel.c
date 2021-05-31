4f38051ec9d485a40820854ec0db44d6f4a53556
Chris Gray <chgray@gmail.com>
2021-05-31 09:22:21 -0700

    move msg to evt because...

--- src/platform/datapath_winkernel.c
+++ src/platform/datapath_winkernel.c
@@ -2090,7 +2090,7 @@ CxPlatDataPathSocketReceive(
         if (IsUnreachableError) {
 #if QUIC_CLOG
             QuicTraceLogVerbose(
-                DatapathUnreachableMsg,
+                DatapathUnreachableEvt,
                 "[sock][%p] Unreachable error from %!ADDR!",
                 Binding,
                 CASTED_CLOG_BYTEARRAY(sizeof(RemoteAddr), &RemoteAddr));
